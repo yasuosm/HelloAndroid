c1f2446217d6dbffe59a69747bee486783fe2bab
An Nguyen <yasuosm@gmail.com>
2015-11-15 19:21:33 +0700

    Intent - Data

--- app/src/main/java/xyz/annt/helloandroid/MainActivity.java
+++ app/src/main/java/xyz/annt/helloandroid/MainActivity.java
@@ -1,20 +1,68 @@
 package xyz.annt.helloandroid;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int PERMISSIONS_REQUEST_CALL_PHONE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Intent secondActivity = new Intent(MainActivity.this, SecondActivity.class);
-//        startActivity(secondActivity);
+//        startSecondActivity();
+//        startActionView();
+        callPhone();
+    }
 
+    public void startSecondActivity() {
+        Intent secondActivity = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(secondActivity);
+    }
+
+    public void startActionView() {
         Intent actionView = new Intent(Intent.ACTION_VIEW);
         startActivity(actionView);
+    }
+
+    public void callPhone() {
+        Intent callPhone = new Intent(Intent.ACTION_CALL);
+        callPhone.setData(Uri.parse("tel:012-345-6789"));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PERMISSIONS_REQUEST_CALL_PHONE);
+                return;
+            }
+        }
+
+        startActivity(callPhone);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CALL_PHONE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    callPhone();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
