6a3ffe08c6dc31da39dc154461b83effa2455425
An Nguyen <yasuosm@gmail.com>
2015-11-22 23:04:51 +0700

    Resources

---
+++ app/src/main/java/xyz/annt/helloandroid/ResourcesActivity.java
@@ -0,0 +1,24 @@
+package xyz.annt.helloandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ResourcesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resources);
+
+//        ImageView image = (ImageView) findViewById(R.id.image_clip);
+//        ClipDrawable drawable = (ClipDrawable) image.getDrawable();
+//        drawable.setLevel(drawable.getLevel() + 7000);
+
+//        ImageView image = (ImageView) findViewById(R.id.image_transition);
+//        TransitionDrawable drawable = (TransitionDrawable) image.getDrawable();
+//        drawable.startTransition(1000);
+
+//        ImageView image = (ImageView) findViewById(R.id.signal_cellular);
+//        image.setImageLevel(3);
+    }
+}
